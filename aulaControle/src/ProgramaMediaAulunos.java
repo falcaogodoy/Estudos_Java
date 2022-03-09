c5844b2bbc190fdec13d4707bd582b41a400c1f3
falcaogodoy <falcaogodoy@gmail.com>
2022-03-09 13:13:49 -0300

    correcao do ex

--- aulaControle/src/ProgramaMediaAulunos.java
+++ aulaControle/src/ProgramaMediaAulunos.java
@@ -5,37 +5,44 @@ public class ProgramaMediaAulunos {
     public static void main(String args[]) {
         Scanner n1 = new Scanner(System.in);
         System.out.println("Digite a primeira Nota :");
-        Double nota1 = n1.nextDouble();
+        Double nota1 = Double.valueOf(n1.nextInt());
 
         Scanner n2 = new Scanner(System.in);
         System.out.println("Digite a segunda Nota :");
-        Double nota2 = n2.nextDouble();
+        Double nota2 = Double.valueOf(n2.nextInt());
 
         Scanner n3 = new Scanner(System.in);
         System.out.println("Digite a Terceira Nota :");
-        Double nota3 = n3.nextDouble();
+        Double nota3 = Double.valueOf(n3.nextInt());
 
         Scanner n4 = new Scanner(System.in);
         System.out.println("Digite a Quarta Nota :");
-        Double nota4 = n4.nextDouble();
+        Double nota4 = Double.valueOf(n4.nextInt());
 
         Double media = (nota1 + nota2 + nota3 + nota4)/4;
 
         System.out.println(media);
 
-        if(media >= 7){
-            System.out.println("Voce Foi APROVADO");
-        }
-        else if (media >= 5) {
-            System.out.println("Voce esta em RECUPERACAO");
-        }
-
-        else if(media <= 5) {
-            System.out.println("VOCE FOI REPROVADO");
-        }
-
+        String mediaSt = getNota(media);
+        System.out.println(mediaSt);
 
     }
 
+    public static String getNota(Double media){
 
+
+        if(media >= 7){
+            return "Voce Foi APROVADO";
+        }
+        else if (media >= 5) {
+            return"Voce esta em RECUPERACAO";
+        }
+
+        else if(media <= 5) {
+            return "VOCE FOI REPROVADO";
+        }
+
+
+        return null;
+    }
 }
